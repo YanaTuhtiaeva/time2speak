38f97ba88c43a24f61c37262d911dd4c2ce177d9
Yana Tuhtiaeva <yana.tsiykhtsiaeva@gmail.com>
2018-10-19 20:49:10 +0300

    button without text

---
+++ timetospeak/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace timetospeak.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "О нас";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Контакты";
+
+            return View();
+        }
+        public ActionResult Feedback ()
+        {
+            ViewBag.Message = "Отзывы";
+
+            return View();
+        }
+        public ActionResult Price ()
+        {
+            ViewBag.Message = "Цены";
+
+            return View();
+        }
+        public ActionResult Test ()
+        {
+            ViewBag.Message = "Тесты";
+
+            return View();
+        }
+    }
+}
