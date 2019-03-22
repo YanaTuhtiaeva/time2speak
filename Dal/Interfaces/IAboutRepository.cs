c80a5701ce044b8cf36d22b822cb9e8e338158ce
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-03-22 16:10:34 +0300

    Add repositiry, entities, context, packages, additional controllers

---
+++ Dal/Interfaces/IAboutRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Dal.Interfaces
+{
+    public interface IAboutRepository<AboutSchool>
+    {
+        AboutSchool Get();
+    }
+}
