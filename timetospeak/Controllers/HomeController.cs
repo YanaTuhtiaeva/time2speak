e058772ed6767d5450362c5f33dfe9a245a24942
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-03-22 15:11:32 +0300

    work items

---
+++ timetospeak/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace timetospeak.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "О нас";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Контакты";
+
+            return View();
+        }
+        public ActionResult Feedback ()
+        {
+            ViewBag.Message = "Отзывы";
+
+            return View();
+        }
+        public ActionResult Price ()
+        {
+            ViewBag.Message = "Цены";
+
+            return View();
+        }
+        public ActionResult Test ()
+        {
+            ViewBag.Message = "Тесты";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Тесты";
+
+            return PartialView();
+        }
+    }
+}
